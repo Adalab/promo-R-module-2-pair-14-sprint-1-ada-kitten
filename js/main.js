cb0768cb9c8213da539d9742e8109e80155eede6
Bruna <brunadasilvabz98@gmail.com>
2022-10-20 11:06:23 +0200

    add new variables

--- js/main.js
+++ js/main.js
@@ -40,3 +40,19 @@ Es una maravilla acariciarle!
 </p>
 </li>`;
 listKitten.innerHTML = kittenOne + kittenTwo + kittenThree;
+
+const kittenOneImage = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
+const kittenOneName = 'Anastacio';
+const kittenOneDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+const kittenRace = 'British Shorthair';
+
+const kittenTwoImage = 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
+const kittenTwoName ='Fiona';
+const kittenTwoDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!';
+const kittenTwoRace = 'British Shorthair';
+
+const kittenThreeImage = 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg';
+const kittenThreeName = 'Cielo';
+const kittenThreeDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.Es una maravilla acariciarle!';
+const kittenThreeRace ='British Shorthair';
+
